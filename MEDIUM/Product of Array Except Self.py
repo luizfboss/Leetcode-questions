b4a8b79e4e281f91d5866861b6254438c7ff9655
luizfboss <88173271+luizfboss@users.noreply.github.com>
2024-01-05 00:12:27 -0600

    new problems updated

---
+++ MEDIUM/Product of Array Except Self.py
@@ -0,0 +1,17 @@
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+def productExceptSelf(self, nums):
+    length = len(nums)
+    products = [1] * length
+    for i in range(1, length):
+        products[i] = products[i-1] * nums[i-1]
+
+    right = nums[-1]
+    for i in range(length-2, -1, -1):
+        products[i] *= right
+        right *= nums[i]
+    
+    return products
+
